eca58255799d1e808ab171b4d5f0906776b3b3ad
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-08-22 10:12:01 +0800

    initial minitest setup and cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "story_branch/git_wrapper"
 
 require "minitest/autorun"
+require "minitest/spec"
+require "minitest/reporters"
+require "story_branch/git"
+
+Minitest::Reporters.use!
